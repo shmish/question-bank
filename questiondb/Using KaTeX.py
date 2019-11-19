d0d3ca1488c0f715103f0435411291109cc76ebf
shmish <dougwsmith@outlook.com>
2019-11-18 21:14:53 -0800

    Add KaTeX functionality

---
+++ questiondb/Using KaTeX.py
@@ -0,0 +1,29 @@
+
+To enter math text into the text editor, put your text in between dollar signs.
+
+To do a subscript, use the _ key.  $$ v_o $$
+
+To do exponents, use the ^ key.  $$ x^2 $$
+
+To have multiple symbols/digits/letters put them inside curly braces.
+
+Here is an example of more complex exponents and subscripts: $$ v_{everything} + x^{allthis} $$
+
+To use the X times symbol use \times:  $$ 1.6 \times 10^{-19} $$
+
+To use fractions use \frac{numerator}{denominator}.
+
+Spaces use \space. Eg: \frac{up \space top}{no space}
+$$ \frac{up \space top}{no space} $$
+
+Vectors are done with \vec .  Example for Fnet: $$ \vec{F}_{net} $$
+
+Theta is \theta , delta is \Delta
+$$ sin\theta = 0.5 $$
+
+Here is an example equation for distance in kinematics:
+$$ \overrightarrow{\Delta x} = v_o t + \frac{1}{2}at^2 $$
+
+For more reference, visit: https://katex.org/docs/supported.html
+
+
